19e74610a8393e9022d8b8a4c6304f56ad07267b
William Thomson <williamthomson@gmail.com>
2020-07-05 20:46:23 +0100

    Initial Commit

---
+++ GeronimoHBS/GeronimoHBS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GeronimoHBS.Startup))]
+namespace GeronimoHBS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
