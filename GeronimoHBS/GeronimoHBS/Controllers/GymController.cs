329b4d8f24f08c93b921c375b16704baed15b7ef
William Thomson <wthoms204@caledonian.ac.uk>
2020-07-22 15:04:49 +0100

    Gym Timetable functionality
    Gym\Timetable.cshtml now displays all the class in a grid structure. Currently displays all classes - regardless of location - inside this grid. TimetableViewModel.cs was created as a structure to store all the GymClasses in the system.

--- GeronimoHBS/GeronimoHBS/Controllers/GymController.cs
+++ GeronimoHBS/GeronimoHBS/Controllers/GymController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeronimoHBS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,11 +21,58 @@ namespace GeronimoHBS.Controllers
         }
 
         // GET: Gym/ClassesDetails/5
-        public ActionResult Timetable(int Id)
+        public ActionResult Timetable()
         {
-            var currentGym = db.Location.Find(Id);
 
-            return View(currentGym);
+            //monday classes
+            var monday = from day1 in db.Timetable
+                         where day1.Day == Day.MONDAY
+                         orderby day1.StartTime
+                         select day1;
+
+            var tuesday = from day2 in db.Timetable
+                          where day2.Day == Day.TUESDAY
+                          orderby day2.StartTime
+                          select day2;
+
+            var wednesday = from day3 in db.Timetable
+                            where day3.Day == Day.WEDNESDAY
+                            orderby day3.StartTime
+                            select day3;
+
+            var thursday = from day4 in db.Timetable
+                           where day4.Day == Day.THURSDAY
+                           orderby day4.StartTime
+                           select day4;
+
+            var friday = from day5 in db.Timetable
+                         where day5.Day == Day.FRIDAY
+                         orderby day5.StartTime
+                         select day5;
+
+            var saturday = from day6 in db.Timetable
+                           where day6.Day == Day.SATURDAY
+                           orderby day6.StartTime
+                           select day6;
+
+            var sunday = from day7 in db.Timetable
+                         where day7.Day == Day.SUNDAY
+                         orderby day7.StartTime
+                         select day7;
+
+
+            TimetableViewModel slvm = new TimetableViewModel()
+            {
+                Monday = monday.ToList(),
+                Tuesday = tuesday.ToList(),
+                Wednesday = wednesday.ToList(),
+                Thursday = thursday.ToList(),
+                Friday = friday.ToList(),
+                Saturday = saturday.ToList(),
+                Sunday = sunday.ToList(),
+            };
+
+            return View(slvm);
         }
         
     }
---
+++ GeronimoHBS/GeronimoHBS/Models/TimetableViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace GeronimoHBS.Models
+{
+    public class TimetableViewModel
+    {
+        public List<Timetable> Monday { get; set; }
+        public List<Timetable> Tuesday { get; set; }
+        public List<Timetable> Wednesday { get; set; }
+        public List<Timetable> Thursday { get; set; }
+        public List<Timetable> Friday { get; set; }
+        public List<Timetable> Saturday { get; set; }
+        public List<Timetable> Sunday { get; set; }
+    }
+
+}
