8293a3187a5ef09d84513d00ea05d97303ec906e
William Thomson <wthoms204@caledonian.ac.uk>
2020-08-01 16:06:52 +0100

    Tab functionality on Dining/Index.cshtml
    Tab functionality on Dining/Index.cshtml + Moved inline javascript from Timetable.cshtml to its own js file + Styled Dining/index.cshtml a little.

---
+++ GeronimoHBS/GeronimoHBS/Scripts/GeronimoScripts/geronimo/tabContent.js
@@ -0,0 +1,13 @@
+﻿function showGymClasses(evt, gymClassName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("timetableColumnTitle");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" activeDaySelected", "");
+    }
+    document.getElementById(gymClassName).style.display = "block";
+    evt.currentTarget.className += " activeDaySelected";
+}
